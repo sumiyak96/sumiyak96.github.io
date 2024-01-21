54c39bec775d74a436ed13f585f1d6d479c2855f
sumiyaaax <wave_soul14@yahoo.co.jp>
2024-01-21 15:18:23 +0900

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from './components/common/header/Header';
-// 他の必要なコンポーネントをインポート
+import Footer from './components/common/footer/Footer';
+import HeroSection from './components/sections/hero/HeroSection';
+import About from './components/sections/about/About';
+import Works from './components/sections/works/Works';
+import Skills from './components/sections/skills/Skills';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
-      {/* 他のセクションやコンポーネント */}
+      <HeroSection />
+      <About />
+      <Works />
+      <Skills />
+      <Footer />
     </div>
   );
 }
