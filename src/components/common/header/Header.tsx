ebedc40977c7e27b49dfa1725b1b4144df976ba0
sumiyaaax <wave_soul14@yahoo.co.jp>
2024-01-21 17:18:55 +0900

    update header scrollToSection

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -20,17 +20,34 @@ const Header: React.FC = () => {
     };
   }, []);
 
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
-    <AppBar position="fixed" className={`${styles.header} ${scrolled ? styles.scrolled : ''}`}>
+    <AppBar position="fixed" className={`${styles.header} ${scrolled ? styles.scrolled : ''}`}
+      sx={{ color: 'black' }}>
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          My Portfolio
+        <Typography sx={{ flexGrow: 1 }}>
+          <span
+            style={{ cursor: 'pointer' }}
+            onClick={scrollToTop}
+          >
+            My Portfolio
+          </span>
         </Typography>
-        <Button color="inherit" href="#top">Top</Button>
-        <Button color="inherit" href="#about">About</Button>
-        <Button color="inherit" href="#works">Works</Button>
-        <Button color="inherit" href="#skills">Skills</Button>
-        <Button color="inherit" href="#contacts">Contacts</Button>
+        <Button color="inherit" onClick={() => scrollToSection('top')}>Top</Button>
+        <Button color="inherit" onClick={() => scrollToSection('about')}>About</Button>
+        <Button color="inherit" onClick={() => scrollToSection('works')}>Works</Button>
+        <Button color="inherit" onClick={() => scrollToSection('skills')}>Skills</Button>
+        <Button color="inherit" onClick={() => scrollToSection('contacts')}>Contacts</Button>
       </Toolbar>
     </AppBar>
   );
