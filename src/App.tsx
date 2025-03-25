f0b843557c329cba451dfd244147e7e0180e9e3c
sumiyak96 <wave_soul14@yahoo.co.jp>
2025-03-25 22:52:04 +0700

    create Experience Section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from './components/common/footer/Footer';
 import HeroSection from './components/sections/hero/HeroSection';
 import About from './components/sections/about/About';
 import Service from './components/sections/service/Service';
+import Experience from './components/sections/experience/Experience';
 import Works from './components/sections/works/Works';
 import Skills from './components/sections/skills/Skills';
 
@@ -15,6 +16,7 @@ const App: React.FC = () => {
       <HeroSection />
       <About />
       <Service />
+      <Experience />
       <Works />
       <Skills />
       <Footer />
