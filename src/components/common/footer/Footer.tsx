07a845cf8bbe24675fa9e387d570bf6ab0ff9cee
sumiyak96 <wave_soul14@yahoo.co.jp>
2025-03-26 19:04:55 +0700

    refactoring

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.scss';
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <p>&copy; {new Date().getFullYear()} Your Name or Company Name. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Sumiya Kurihara. All rights reserved.</p>
       {/* ここに追加の情報やリンクを配置できます */}
     </footer>
   );
